c33dba286dddd8f90ce84a0cdb37282ed7825e67
tracer-nju <tracer@nju.edu.cn>
2023-07-01 20:50:32 +0800

    Linux wsb 5.19.0-46-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jun 21 15:35:31 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:50:32 up  6:29,  3 users,  load average: 1.21, 1.03, 1.06

--- amgame/src/video.c
+++ amgame/src/video.c
@@ -32,7 +32,7 @@ void splash() {
       //   continue;
       // }
       if ((x & 1) ^ (y & 1)) {
-        draw_tile(x * SIDE, y * SIDE, SIDE, SIDE, 0xfffeff); // white
+        draw_tile(x * SIDE, y * SIDE, SIDE, SIDE, 0xeeeeee); // white
       }
     }
   }
