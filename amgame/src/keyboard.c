ec0b6ff72dd91bb81728b10c724585fd7ae02184
tracer-nju <tracer@nju.edu.cn>
2023-07-02 15:05:27 +0800

    Linux wsb 5.19.0-46-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jun 21 15:35:31 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:05:27 up  1:01,  1 user,  load average: 1.60, 1.47, 1.25

--- amgame/src/keyboard.c
+++ amgame/src/keyboard.c
@@ -5,17 +5,15 @@
 static const char *key_names[] = {
   AM_KEYS(KEYNAME)
 };
-
 void print_key() {
-  // int dir[2] = {0, 0};
   AM_INPUT_KEYBRD_T event = { .keycode = AM_KEY_NONE };
   ioe_read(AM_INPUT_KEYBRD, &event);
   if (event.keycode != AM_KEY_NONE && event.keydown) {
     puts("Key pressed: ");
     puts(key_names[event.keycode]);
     puts("\n");
-    int dir[] = {1, 1};
-    switch(event.keycode){
+    int dir[] = {0, 0};
+    switch(*key_names[event.keycode]){
       case 'W':
           dir[0] = -1;
       break;
