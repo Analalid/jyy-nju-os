0591c00ddcc23eba046bdab762d9f1d4c5abe19a
tracer-nju <tracer@nju.edu.cn>
2023-07-03 13:47:12 +0800

    Linux wsb 5.19.0-46-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jun 21 15:35:31 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:47:12 up  3:29,  3 users,  load average: 0.84, 0.74, 0.64

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 #include <assert.h>
-
+void mkTree(char * filename);
 int main(int argc, char *argv[]) {
+  char *processes[65536];
   for (int i = 0; i < argc; i++) {
     assert(argv[i]);
     printf("argv[%d] = %s\n", i, argv[i]);
   }
   assert(!argv[argc]);
+  mkTree(argv[1]);
+  // dfs(1);
   return 0;
 }
-// void Dfs(){
+// void dfs(int node){
 //   FILE *fp = fopen(filename, "r");
 //   if (fp) {
 //   // 用fscanf, fgets等函数读取
@@ -18,3 +21,14 @@ int main(int argc, char *argv[]) {
 //   // 错误处理
 //   }
 // }
+void mkTree(char * filename){
+  FILE *fp = fopen(filename, "r");
+  if (fp) {
+  // 用fscanf, fgets等函数读取
+  puts(filename);
+  fclose(fp);
+  } else {
+  // 错误处理
+  puts("error!!!!!!!!!!!!!!!!!!");
+  }
+}
