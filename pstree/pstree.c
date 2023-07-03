7a6ecacead9c155e813d8b513acfbe30989919eb
tracer-nju <tracer@nju.edu.cn>
2023-07-03 21:52:24 +0800

    Linux wsb 5.19.0-46-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jun 21 15:35:31 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:52:24 up 11:34,  5 users,  load average: 1.40, 0.72, 0.46

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -74,7 +74,7 @@ void mkTree(char *dirName, int pid){
   }
 
   FILE *fp = fopen(filename, "r");
-  char buf[1024];ashfnasjkfhasjnasjkdfasjdasnfas
+  char buf[1024];
   if (fp) {
   // 用fscanf, fgets等函数读取
     for(int i = 0; i < 7; ++i){
