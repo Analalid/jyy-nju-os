d767107c9c13875fb3464ea0fbb2e596070bcbb1
tracer-nju <tracer@nju.edu.cn>
2023-07-03 21:07:57 +0800

    Linux wsb 5.19.0-46-generic #47~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jun 21 15:35:31 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:07:57 up 10:50,  5 users,  load average: 0.89, 0.84, 0.81

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -30,7 +30,7 @@ int main(int argc, char *argv[]) {
 }
 void dfs(int curNode, int depth){
   for(int i = 0; i < depth; ++i){
-    printf("\t");
+    printf("\t├─");
   }
   printf("%d", curNode);
   if(processesSonCount[curNode] == 0){
