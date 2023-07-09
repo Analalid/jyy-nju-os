ed23d4b1fffedda4431bb6be18d24a015e974a26
tracer-nju <tracer@nju.edu.cn>
2023-07-09 21:28:18 +0800

    Linux LAPTOP-IRKK0LSS 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:28:18 up  2:41,  0 users,  load average: 0.00, 0.00, 0.00

--- libco/co.c
+++ libco/co.c
@@ -1,11 +1,25 @@
 #include "co.h"
 #include <stdlib.h>
 
+#define NTHREAD 64
+enum { T_FREE = 0, T_LIVE, T_DEAD, };
+
 struct co {
+  int status;
+  pthread_t thread;
+  void (*entry)(int);
 };
 
 struct co *co_start(const char *name, void (*func)(void *), void *arg) {
-  return NULL;
+  struct co* = res;
+  *res = (struct co) {
+    .status = T_LIVE,
+    .entry = func,
+  };
+
+  pthread_create(&(res->thread), NULL, NULL, res);
+
+  return res;
 }
 
 void co_wait(struct co *co) {
