1548e5b6957cdf4b0d2845bec60f59d8dac698ff
tracer-nju <tracer@nju.edu.cn>
2023-07-09 22:05:42 +0800

    Linux LAPTOP-IRKK0LSS 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:05:42 up  3:19,  0 users,  load average: 0.00, 0.00, 0.00

--- libco/co.c
+++ libco/co.c
@@ -17,7 +17,7 @@ void *wrapper(void *arg) {
 }
 
 struct co *co_start(const char *name, void (*func)(void *), void *arg) {
-  printf("\n***************************\n");
+  puts("\n***************************\n");
   struct co* res = NULL;
   *res = (struct co) {
     .status = T_LIVE,
