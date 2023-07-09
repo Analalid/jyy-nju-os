24e6a2b699f4d5b3102d5914f3d0485cc91e2797
tracer-nju <tracer@nju.edu.cn>
2023-07-09 21:41:42 +0800

    Linux LAPTOP-IRKK0LSS 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:41:42 up  2:55,  0 users,  load average: 0.04, 0.01, 0.00

--- libco/tests/main.c
+++ libco/tests/main.c
@@ -28,10 +28,10 @@ static void work(void *arg) {
 }
 
 static void test_1() {
+    printf("=======");
 
     struct co *thd1 = co_start("thread-1", work, "X");
     struct co *thd2 = co_start("thread-2", work, "Y");
-    printf("=======");
     co_wait(thd1);
     co_wait(thd2);
 
