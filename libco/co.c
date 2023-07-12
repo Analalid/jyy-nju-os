3e04a0106a4a2ebb43e7fc85f1c4a2d6189df80c
tracer-nju <tracer@nju.edu.cn>
2023-07-12 20:52:26 +0800

    Linux LAPTOP-IRKK0LSS 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:52:26 up 14 min,  0 users,  load average: 0.15, 0.07, 0.02

--- libco/co.c
+++ libco/co.c
@@ -37,13 +37,9 @@ struct co *co_start(const char *name, void (*func)(void *), void *arg) {
   void *sp;
   asm volatile ("mov %%rsp, %0" : "=r" (sp));
   asm volatile (
-#if __x86_64__
     "movq %0, %%rsp; movq %2, %%rdi; jmp *%1" 
     : 
     : "b"((uintptr_t)sp),     "d"(func), "a"(arg)
-#else
-    "movl %0, %%esp; movl %2, 4(%0); jmp *%1" : : "b"((uintptr_t)sp - 8), "d"(entry), "a"(arg)
-#endif
   );
   printf("=========2");
   return NULL;
