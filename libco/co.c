4f263a90033e7e279e536de18c70a3cb7e7ab950
tracer-nju <tracer@nju.edu.cn>
2023-07-13 17:02:30 +0800

    Linux LAPTOP-IRKK0LSS 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:02:30 up 18:32,  0 users,  load average: 0.00, 0.10, 0.07

--- libco/co.c
+++ libco/co.c
@@ -49,10 +49,10 @@ struct co *co_start(const char *name, void (*func)(void *), void *arg) {
   printf("=========in");
   if(setjmp(my_co->context) == 0){
     //初次初始化
-    printf("init->%s", my_co->name);  
+    // printf("init->%s", my_co->name);  
   }else{
     //error!
-    printf("cann't init->%s twice !!!", my_co->name);  
+    // printf("cann't init->%s twice !!!", my_co->name);  
   }
   printf("=========out");
   return my_co;
