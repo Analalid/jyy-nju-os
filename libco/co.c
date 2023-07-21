9884a703f21f2cdbb29bcf093ff094bcb8b7785b
tracer-nju <tracer@nju.edu.cn>
2023-07-21 15:14:18 +0800

    Linux LAPTOP-IRKK0LSS 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:14:18 up 27 min,  0 users,  load average: 0.29, 0.06, 0.02

--- libco/co.c
+++ libco/co.c
@@ -1,23 +1,17 @@
 #include "co.h"
-#include <stdint.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include <setjmp.h>
-#include <assert.h>
 #include <string.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <time.h>
 
-#define KB *1024
-#define STACK_SIZE (64 KB)
-//#define DEBUG
-#ifdef DEBUG
-#define Log(format, ...) \
-    printf("\33[1;34m[%s,%d,%s] " format "\33[0m\n", \
-        __FILE__, __LINE__, __func__, ## __VA_ARGS__)
-#else
-#define Log(format,...)
-#endif
+//支持unit_8
+// typedef unsigned char uint8_t;
+//支持STACK_SIZE
+// #define STACK_SIZE 1024
+
+#define NTHREAD 64
+
+typedef unsigned long int	uintptr_t;
 
 static inline void stack_switch_call(void *sp, void *entry, uintptr_t arg) {
   asm volatile (
@@ -28,7 +22,6 @@ static inline void stack_switch_call(void *sp, void *entry, uintptr_t arg) {
 #endif
   );
 }
-
 enum co_status {
   CO_NEW = 1, // 新创建，还未执行过
   CO_RUNNING, // 已经执行过
@@ -36,103 +29,70 @@ enum co_status {
   CO_DEAD,    // 已经结束，但还未释放资源
 };
 struct co {
-  const char *name;
+  char *name;
   void (*func)(void *); // co_start 指定的入口地址和参数
   void *arg;
-  void *stackptr;
   enum co_status status;  // 协程的状态
-  jmp_buf context; // 寄存器现场 (setjmp.h)
-  struct co *next;
-  struct co *prev;
-  struct co *waiter;
-  uint8_t stack[STACK_SIZE] __attribute__ ((aligned(16)));// 协程的堆栈
-};
+  struct co *    waiter;  // 是否有其他协程在等待当前协程
+  jmp_buf        context; // 寄存器现场 (setjmp.h)
+  // uint8_t        stack[STACK_SIZE]; // 协程的堆栈
 
-struct co *co_main,*co_current;
-int co_num=0;
+};
+struct co* co_main, *co_current;
 
 __attribute__((constructor)) void co_init() {
-  co_main=malloc(sizeof(struct co));
+  co_main = malloc(sizeof(struct co));
   co_main->name="main";
-  co_main->stackptr=co_main->stack+sizeof(co_main->stack);
   co_main->status=CO_RUNNING;
-  memset(co_main->stack,0,sizeof(co_main->stack));
-  co_main->next=co_main;
-  co_main->prev=co_main;
-  co_current=co_main;
-  co_num+=1;
-}
-
-void debugprint(){
-  struct co *p=co_main;
-  printf("debugprint begin\n");
-  for(int i=0;i<co_num;i++){
-    Log("co:%s,co_num:%d",p->name,co_num);
-    p=p->next;
-  }
-  printf("debugprint end\n");
+  co_main->waiter=co_main;
+  co_current = co_main;
 }
 
 
-struct co* co_start(const char *name, void (*func)(void *), void *arg){
-  struct co *co_new=malloc(sizeof(struct co));
-  co_new->name=name;
-  co_new->func=func;
-  co_new->arg=arg;
-  co_new->status=CO_NEW;
-  co_new->stackptr=co_new->stack+sizeof(co_new->stack);
-  memset(co_new->stack,0,sizeof(co_new->stack));
-  struct co *prev=co_main->prev;
-  co_new->next=co_main;
-  co_main->prev=co_new;
-  co_new->prev=prev;
-  prev->next=co_new;
-  co_num+=1;
-  //debugprint();
-  return co_new;
-};
 
-void wrapper(){
-  co_current->status=CO_RUNNING;
+void *wrapper(void *arg) {
+  co_current->status = CO_RUNNING;
   co_current->func(co_current->arg);
-  co_current->status=CO_DEAD;
-  if(co_current->waiter!=NULL){
-    co_current->waiter->status=CO_RUNNING;
-    co_current->waiter=NULL;
+  co_current->status = CO_DEAD;
+  if(co_current->waiter != NULL){
+    co_current->waiter->status = CO_RUNNING;
+    co_current->waiter = NULL;
   }
   co_yield();
+  return NULL;
 }
 
-void co_yield(){
-  int val=setjmp(co_current->context);
-  if(val==0){
-    struct co *next=co_current->next;
-    while(next->status==CO_WAITING || next->status==CO_DEAD) next=next->next;
-    co_current=next;
-    if(next->status==CO_NEW){
-      if(sizeof(void*)==4) stack_switch_call(next->stackptr,wrapper,(uintptr_t)NULL);
-      else{
-        asm volatile("mov %0,%%rsp"::"b"((uintptr_t)next->stackptr));
-        wrapper();
-      }
-    }
-    else{
-      longjmp(next->context,0);
-    } 
+struct co *co_start(const char *name, void (*func)(void *), void *arg) {
+  struct co *my_co = malloc(sizeof(struct co));
+  my_co->name = malloc(sizeof(name));
+  strcpy(my_co->name, name);
+  my_co->func = func;
+  my_co->arg = arg;
+  my_co->status = CO_NEW;
+  my_co->waiter = NULL;
+  printf("=========in\n");
+  if(setjmp(my_co->context) == 0){
+    //初次初始化
+    printf("init->%s\n", my_co->name);
+  }else{
+    //error!
+    printf("cann't init->%s twice !!!\n", my_co->name);
   }
-};
-void co_wait(struct co *co){
-  if(co->status==CO_DEAD){
-    Log("free %s",co->name);
-    struct co *prev=co->prev;
-    struct co *next=co->next;
-    prev->next=next;
-    next->prev=prev;
-    free(co);
-  }
-  else{
-    co->waiter=co_current;
-    co_current->status=CO_WAITING;
+  printf("=========out\n");
+  return my_co;
+}
+
+void co_wait(struct co *co) {
+  //直到这个协程还没死，就一直循环
+  while(co->status != CO_DEAD){
     co_yield();
   }
-};
+  //死了再释放
+  free(co);
+  return;
+}
+
+void co_yield() {
+  
+  return;
+}
