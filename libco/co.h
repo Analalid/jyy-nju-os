aae38eaca7e1593659de702963207facf4e86f10
tracer-nju <tracer@nju.edu.cn>
2023-09-03 07:59:46 +0800

    Linux LAPTOP-IRKK0LSS 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  07:59:46 up 23 min,  0 users,  load average: 0.01, 0.03, 0.00

--- libco/co.h
+++ libco/co.h
@@ -1,31 +1,4 @@
-#ifndef __CO_C__
-#define __CO_C__
-#define STACK_SIZE	1024*256
-
-#include <setjmp.h>
-
-typedef enum {
-	CO_NEW = 1,	// new coroutine,还未执行
-	CO_RUNNING, // 已经执行过的coroutinue
-	CO_WAITTING,	// 在co_wait上等待 
-	CO_DEAD		// 已经结束，但还未释放资源 	
-}co_status;
-
-typedef struct {
-	char *name;
-	int cid;
-	void (*func)(void *arg);
-	void *arg;
-
-	co_status status;	struct co *waiter;	// 是否有其他coroutine在等待当前coroutine
-	jmp_buf *env;	// context
-	unsigned char stack[STACK_SIZE];	// stack of coroutine
-}co;
-
-// function prototype
-void initCoroutinr();
-co* co_start(const char *name, void (*func)(void *), void *arg);
+struct co* co_start(const char *name, void (*func)(void *), void *arg);
 void co_yield();
-void co_wait(co *co);
+void co_wait(struct co *co);
 
-#endif
