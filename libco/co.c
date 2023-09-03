8891329ca1d9ccfebca6fd0177acf48e39b94a22
tracer-nju <tracer@nju.edu.cn>
2023-09-03 08:21:32 +0800

    Linux LAPTOP-IRKK0LSS 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  08:21:32 up 44 min,  0 users,  load average: 0.08, 0.06, 0.01

--- libco/co.c
+++ libco/co.c
@@ -35,12 +35,12 @@ enum co_status {
 };
 struct co {
   char *name;
-  void (*func)(void *); // co_start 指定的入口地址和参数
+  void (*func)(void *arg); // co_start 指定的入口地址和参数
   void *arg;
   enum co_status status;  // 协程的状态
   struct co *    pre;
   struct co *    next;
-  jmp_buf        context; // 寄存器现场 (setjmp.h)
+  jmp_buf        *context; // 寄存器现场 (setjmp.h)
   uint8_t        stack[STACK_SIZE]; // 协程的堆栈
 
 };
