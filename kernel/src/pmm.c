93d822f6140541169b19be5b90c0d05aedaa3583
tracer-nju <tracer@nju.edu.cn>
2023-09-07 14:04:29 +0800

    Linux LAPTOP-IRKK0LSS 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:04:29 up  2:02,  0 users,  load average: 0.12, 0.05, 0.01

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -10,7 +10,9 @@ static void kfree(void *ptr) {
 static void pmm_init() {
   uintptr_t pmsize = ((uintptr_t)heap.end - (uintptr_t)heap.start);
   printf("Got %d MiB heap: [%p, %p)\n", pmsize >> 20, heap.start, heap.end);
+  
 }
+
 
 MODULE_DEF(pmm) = {
   .init  = pmm_init,
