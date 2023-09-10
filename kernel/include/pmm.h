e39acea33d8acbdbc46980a759ff093494d50e16
tracer-nju <tracer@nju.edu.cn>
2023-09-10 19:49:18 +0800

    Linux LAPTOP-IRKK0LSS 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:49:18 up  3:51,  0 users,  load average: 0.03, 0.02, 0.00

---
+++ kernel/include/pmm.h
@@ -0,0 +1,5 @@
+static int get2PowSize(int size);
+static void *kalloc(size_t size);
+static void kfree(void *ptr);
+static void pmm_init();
+static void buddy_init(uintptr_t start, uintptr_t end);
--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -1,6 +1,37 @@
 #include <common.h>
+#include <pmm.h>
+#define MB (1 << 20)
+#define MAXSIZE (16 * MB) //16MiB 能接受的最大内存分配
+#define MINSIZE (1 << 12)
+#define MAX_BUDDY_BLOCK_SIZE (16 * MB)//伙伴系统最大块
+
+// #define uintptr_t int
+//BUDDYS数组的长度
+static int BUDDY_SIZE;
+//空闲列表结构体
+typedef struct node_t{
+   int size;
+   struct node_t * next;
+} node_t;
+typedef struct buddy_block{
+  char *head;
+  int status;
+  //锁
+}buddy_block;
+//伙伴系统数组
+struct buddy_block *buddysArray;
+//向上对齐到最接近的幂, tips:(之后替换为二分解法)
+static int get2PowSize(int size){
+  int newSize = 1;
+  while(newSize < size) newSize <<= 1;
+  return newSize;
+}
 //对应实验要求中的 kalloc；
 static void *kalloc(size_t size) {
+  //超过MAXSIZE的是不合法的申请
+  if(size >= MAXSIZE || size < 0) return NULL;
+  //组装头部
+  // size = size + sizeof(node_t); 
   return NULL;
 }
 //对应实验要求中的 kfree。
@@ -10,14 +41,21 @@ static void kfree(void *ptr) {
 static void pmm_init() {
   uintptr_t pmsize = ((uintptr_t)heap.end - (uintptr_t)heap.start);
   printf("Got %d MiB heap: [%p, %p)\n", pmsize >> 20, heap.start, heap.end);
-  
+  get2PowSize(10);
+  //伙伴系统的初始化
+  buddy_init((uintptr_t)heap.start, (uintptr_t)heap.end);
 }
-
+static void buddy_init(uintptr_t start, uintptr_t end){
+  BUDDY_SIZE = (end - start) / MAX_BUDDY_BLOCK_SIZE;
+  for(uintptr_t i = start; i < end; ++i){
+    buddysArray = (buddy_block*)i;
+    printf("%p", (uintptr_t)i);
+  }
+}
 //1 
 //2 3 
 //4 5 6 7
 //8 9 10 11 12 13 14 15
-//
 MODULE_DEF(pmm) = {
   .init  = pmm_init,
   .alloc = kalloc,
