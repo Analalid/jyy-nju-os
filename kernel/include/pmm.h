dd21d762e05bcfe2479c999646c5be0d2b6fb698
Analalid <2315932049@qq.com>
2023-09-11 21:00:49 +0800

    finished first step

--- kernel/include/pmm.h
+++
@@ -1,6 +0,0 @@
-static int get2PowSize(int size);
-static void *kalloc(size_t size);
-static void kfree(void *ptr);
-static void pmm_init();
-static void buddy_init(uintptr_t start, uintptr_t end);
-static int dfs(size_t size, size_t curSize);
--- kernel/src/test/test.c
+++ kernel/src/test/test.c
@@ -1,4 +1,6 @@
-// #include <stdio.h>
+#include <kernel.h>
+#include <klib.h>
+
 // #ifndef TEST
 // // 框架代码中的 pmm_init (在 AbstractMachine 中运行)
 // static void pmm_init() {
@@ -7,11 +9,11 @@
 //   printf("Got %d MiB heap: [%p, %p)\n", pmsize >> 20, heap.start, heap.end);
 // }
 // #else
-// // 测试代码的 pmm_init ()
-// static void pmm_init() {
-//   char *ptr  = malloc(HEAP_SIZE);
-//   heap.start = ptr;
-//   heap.end   = ptr + HEAP_SIZE;
-//   printf("Got %d MiB heap: [%p, %p)\n", HEAP_SIZE >> 20, heap.start, heap.end);
-// }
+// 测试代码的 pmm_init ()
+static void pmm_init() {
+  char *ptr  = malloc(HEAP_SIZE);
+  heap.start = ptr;
+  heap.end   = ptr + HEAP_SIZE;
+  printf("Got %d MiB heap: [%p, %p)\n", HEAP_SIZE >> 20, heap.start, heap.end);
+}
 // #endif
