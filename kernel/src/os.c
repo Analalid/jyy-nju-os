455adb77e50fd6b115fed42c95f5d6e3c9c48a5d
tracer-nju <tracer@nju.edu.cn>
2023-09-12 22:04:23 +0800

    Linux LAPTOP-IRKK0LSS 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:04:23 up  3:01,  0 users,  load average: 0.05, 0.30, 0.32

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -25,9 +25,9 @@ static void os_init() {
 }
 
 static void os_run() {
-  for (const char *s = "Hello World from CPU #*\n"; *s; s++) {
-    putch(*s == '*' ? '0' + cpu_current() : *s);
-  }
+  // for (const char *s = "Hello World from CPU #*\n"; *s; s++) {
+  //   putch(*s == '*' ? '0' + cpu_current() : *s);
+  // }
    pmm_test();
   while (1) ;
 }
