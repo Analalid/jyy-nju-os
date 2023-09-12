bf469de366cc4956b5e2f4978eb789623c583103
tracer-nju <tracer@nju.edu.cn>
2023-09-12 20:25:02 +0800

    Linux LAPTOP-IRKK0LSS 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:25:02 up  1:22,  0 users,  load average: 0.27, 0.08, 0.02

--- kernel/test/common.h
+++
@@ -1,9 +0,0 @@
-#include <kernel.h>
-#include <stdio.h>
-#include <stdint.h>
-#define ROUNDUP(a, sz)      ((((uintptr_t)a) + (sz) - 1) & ~((sz) - 1))
-
-typedef struct {
-	void *start, *end;
-} Area;
-Area heap;
