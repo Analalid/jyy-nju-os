3ec8e308dcbf0591e40f4a37388eb07b93269a83
tracer-nju <tracer@nju.edu.cn>
2023-09-12 22:09:24 +0800

    Linux LAPTOP-IRKK0LSS 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:09:24 up  3:06,  0 users,  load average: 0.05, 0.11, 0.23

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -100,10 +100,10 @@ static void *kalloc(size_t size) {
   if(size >= MAXSIZE || size < 0) return NULL;
   //TODO
   //尚未完成slab时的代替方案
+  printf("%d\n", &size);
   size = size > MIN_BUDDY_BLOCK_SIZE ? size : MIN_BUDDY_BLOCK_SIZE;
   //向上对齐
   size = get2PowSize(size);
-  printf("%d\n", &size);
   //通过伙伴系统分配
   for(int i = 0; i < BUDDY_SIZE; ++i){
     void* res = balloc(size, i);
