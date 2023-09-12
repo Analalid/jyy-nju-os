c7afcd2a96786a9e82f2ef68be88048765879277
tracer-nju <tracer@nju.edu.cn>
2023-09-12 21:46:49 +0800

    Linux LAPTOP-IRKK0LSS 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:46:49 up  2:44,  0 users,  load average: 0.52, 0.31, 0.15

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -94,7 +94,6 @@ static void* balloc(size_t size, int idx){
 }
 //对应实验要求中的 kalloc；
 static void *kalloc(size_t size) {
-  printf("kalloc\n");
   //超过MAXSIZE的是不合法的申请
   if(size >= MAXSIZE || size < 0) return NULL;
   //TODO
