a04910f48ee8aa84b17d8040ef173369c5ebd190
tracer-nju <tracer@nju.edu.cn>
2023-09-13 21:43:38 +0800

    Linux LAPTOP-IRKK0LSS 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:43:38 up 17:03,  0 users,  load average: 0.00, 0.01, 0.01

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -9,16 +9,16 @@
 #define free(addr) pmm->free(addr)
 void alloc_and_free() {
 	void *a;
+  a = alloc(MAXSIZE); 
   a = alloc(MAXSIZE); free(a);
-  a = alloc(MAXSIZE); free(a);
-	// a = alloc(1);
-	a = alloc(128);				free(a);
-	a = alloc(MAXSIZE / 2);			free(a);
-  a = alloc(4096);			free(a);
-	a = alloc(1 << 16);		free(a);
-  a = alloc(1 << 16);		free(a);
-	a = alloc(1 << 17);		free(a);
-  a = alloc(1 << 17);		free(a);
+	// // a = alloc(1);
+	// a = alloc(128);				free(a);
+	// a = alloc(MAXSIZE / 2);			free(a);
+  // a = alloc(4096);			free(a);
+	// a = alloc(1 << 16);		free(a);
+  // a = alloc(1 << 16);		free(a);
+	// a = alloc(1 << 17);		free(a);
+  // a = alloc(1 << 17);		free(a);
 }
 void pmm_test(){
   alloc_and_free();
