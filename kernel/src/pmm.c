b9533e720457a6645fedd9c310eeaccab9bc485a
tracer-nju <tracer@nju.edu.cn>
2023-09-13 11:02:03 +0800

    Linux LAPTOP-IRKK0LSS 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:02:03 up  6:22,  0 users,  load average: 0.16, 0.14, 0.09

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -105,11 +105,11 @@ static void *kalloc(size_t size) {
   size = size > MIN_BUDDY_BLOCK_SIZE ? size : MIN_BUDDY_BLOCK_SIZE;
   //向上对齐
   size = get2PowSize(size);
-  int xxxx = 1;
-  printf("%d\n", xxxx);
+  // int xxxx = 1;
+  // printf("%d\n", xxxx);
   //通过伙伴系统分配
-  for(int i = 0; i < BUDDY_SIZE; i += 1){
-  printf("%d====\n", i);
+  for(int i = 0; i < BUDDY_SIZE; i++){
+    printf("%d===dfghxcfuhg=\n", i);
     void* res = balloc(size, i);
     if(res != (void*)0) return (void*)res;
   }
