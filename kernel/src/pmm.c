e6968a5f9554e659274a4fa7e2e58e9b21960abb
tracer-nju <tracer@nju.edu.cn>
2023-09-13 10:48:22 +0800

    Linux LAPTOP-IRKK0LSS 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:48:22 up  6:08,  0 users,  load average: 0.06, 0.06, 0.02

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -56,7 +56,6 @@ static void setBuddyHead(uintptr_t idx, buddy_head* buddy_head_base){
 }
 //递归查找，详情看伙伴系统的实现, 无可用空间的时候返回-1, idx用于定位伙伴头数组的位置
 static uintptr_t dfs(size_t size, size_t curSize, void* baseAddr, buddy_head* buddy_head_base, uintptr_t idx){
-    printf("%d\n",idx);
   //找不到可分配的空间了
   if(curSize < size){
     #ifdef BUDDY_SYS_DEBUG
@@ -64,6 +63,7 @@ static uintptr_t dfs(size_t size, size_t curSize, void* baseAddr, buddy_head* bu
     #endif
     return 0;
   }
+  printf("%d\n",idx);
   buddy_head* node = (buddy_head*)(BUDDY_HEAD_START + (idx - 1) * sizeof(buddy_head));
   int status = node->status;
   //内存块完全被占用
