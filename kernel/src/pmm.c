78d81cea5e77e430aeb79448ff7e21152cf5c37c
tracer-nju <tracer@nju.edu.cn>
2023-09-13 20:22:58 +0800

    Linux LAPTOP-IRKK0LSS 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:22:58 up 15:43,  0 users,  load average: 0.22, 0.20, 0.18

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -119,7 +119,7 @@ static void buddy_Block_Init(void* block, int size){
 //递归调用， 释放空间, 成功则返回1
 int searchFull(void* ptr, void* baseBuddy, void *baseBuddyHead, int idx, size_t curSize){
   buddy_head* headAddress = (buddy_head*) baseBuddyHead + (idx - 1) * sizeof(buddy_head);
-  printf("ptr:%p, baseBuddy:%p   status:%lu\n++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n ", ptr, baseBuddy, headAddress->status);
+  printf("ptr:%p, baseBuddy:%p   status:%d\n++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n ", ptr, baseBuddy,(int)headAddress->status);
   if(ptr == baseBuddy && headAddress->status == 2){
     printf("free :%p\n", headAddress);
     //释放空间
