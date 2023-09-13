a78c277ea4186dac7e2e92bedbed7aee4b30be2c
tracer-nju <tracer@nju.edu.cn>
2023-09-13 10:54:04 +0800

    Linux LAPTOP-IRKK0LSS 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:54:04 up  6:14,  0 users,  load average: 0.35, 0.17, 0.05

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -91,7 +91,7 @@ static void* balloc(size_t size, int idx){
   //递归查找
   uintptr_t res = dfs(size, MAX_BUDDY_BLOCK_SIZE, (void*)BUDDY_START + idx * MAX_BUDDY_BLOCK_SIZE, (buddy_head*)(BUDDY_HEAD_START), 1);
   //BUDDY_START + size * res - (BUDDY_END - BUDDY_START)位置是算出来
-  printf("分配到第 %d 的 %d 处\n", idx, res);
+  printf("分配到第 %d 的 %d 处\n", &idx, res);
   return res == 0 ? (void*)0 : (void*)BUDDY_START + size * res - (BUDDY_END - BUDDY_START) + idx * MAX_BUDDY_BLOCK_SIZE;
 }
 //对应实验要求中的 kalloc；
