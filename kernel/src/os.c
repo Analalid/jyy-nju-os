b284a18a35adcb235036e511c6449db981928ba2
tracer-nju <tracer@nju.edu.cn>
2023-09-13 14:58:08 +0800

    Linux LAPTOP-IRKK0LSS 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:58:08 up 10:18,  0 users,  load average: 0.43, 0.22, 0.08

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -7,8 +7,7 @@
 void alloc_and_free() {
 	void *a;
 	a = alloc(1);		
-  printf("%p\n", a);
-  	// free(a);
+  	free(a);
 	// a = alloc(128);				free(a);
 	// a = alloc(4096);			free(a);
 	// a = alloc(1 << 16);		free(a);
