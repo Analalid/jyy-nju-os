97798658948fa58c74903190536ef710cbfeccc9
tracer-nju <tracer@nju.edu.cn>
2023-09-24 22:13:49 +0800

    Linux LAPTOP-IRKK0LSS 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:13:49 up 1 day,  7:10,  0 users,  load average: 0.02, 0.06, 0.01

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -5,11 +5,9 @@
 int main(int argc, char *argv[]) {
   char *exec_argv[] = { "strace", "ls", NULL, };
   char *exec_envp[] = { "PATH=/bin", NULL, };
-  int p = 10000;
-  p = execve("strace",          exec_argv, exec_envp);
-  execve("/bin/strace",     exec_argv, exec_envp);
+  // execve("strace",          exec_argv, exec_envp);
+  // execve("/bin/strace",     exec_argv, exec_envp);
   execve("/usr/bin/strace", exec_argv, exec_envp);
   perror(argv[0]);
-  printf("%d",p);
   exit(EXIT_FAILURE);
 }
