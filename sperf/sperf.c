09e2b51cade98433fed0c9fe61fbad84d73b14b5
tracer-nju <tracer@nju.edu.cn>
2023-09-24 21:27:44 +0800

    Linux LAPTOP-IRKK0LSS 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:27:44 up 1 day,  6:24,  0 users,  load average: 0.23, 0.08, 0.02

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -3,10 +3,9 @@
 #include <unistd.h>
 
 int main(int argc, char *argv[]) {
-  char *exec_argv[] = { "sudo strace", "ls", NULL, };
+  char *exec_argv[] = { "strace", "ls", NULL, };
   char *exec_envp[] = { "PATH=/bin", NULL, };
-  execve("/", exec_argv, exec_envp);
-  // execve("strace",          exec_argv, exec_envp);
+  execve("strace",          exec_argv, exec_envp);
   // execve("/bin/strace",     exec_argv, exec_envp);
   // execve("/usr/bin/strace", exec_argv, exec_envp);
   perror(argv[0]);
