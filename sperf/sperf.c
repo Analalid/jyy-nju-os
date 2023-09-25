89244e045de5dbfa48d60efc7b0f31aabfeddb62
tracer-nju <tracer@nju.edu.cn>
2023-09-25 17:00:08 +0800

    Linux LAPTOP-IRKK0LSS 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:00:08 up 1 day, 13:20,  0 users,  load average: 0.17, 0.18, 0.10

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -8,7 +8,7 @@ int main(int argc, char *argv[]) {
   char *exec_envp[] = { "PATH=/bin", NULL, };
   // execve("strace",          exec_argv, exec_envp);
   // execve("/usr/bin/strace", exec_argv, exec_envp);
-    close(STDOUT_FILENO);
+  close(STDOUT_FILENO);
   int fd = open("./sperf_tmp.output", O_CREAT|O_WRONLY|O_TRUNC,S_IRWXU);  
   if(fd < 0) perror("open file faild!\n");
   int p = fork();
@@ -17,8 +17,8 @@ int main(int argc, char *argv[]) {
   }else if(p > 0){
     wait(NULL);
   }else{
-    // execve("/bin/strace",     exec_argv, exec_envp);
-    printf("Hello, I am son\n");
+    execve("/bin/strace",     exec_argv, exec_envp);
+    // printf("Hello, I am son\n");
   }
   perror(argv[0]);
   exit(EXIT_FAILURE);
