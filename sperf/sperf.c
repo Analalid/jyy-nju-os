06ae86d3dded8b2390d5c64f01250f499e409101
tracer-nju <tracer@nju.edu.cn>
2023-09-25 18:12:59 +0800

    Linux LAPTOP-IRKK0LSS 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:12:59 up 1 day, 14:33,  0 users,  load average: 0.08, 0.03, 0.00

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -19,7 +19,7 @@ int main(int argc, char *argv[]) {
     wait(NULL);
   }else{
     printf("Hello, I am son\n");
-    execve("",     exec_argv, exec_envp);
+    execve("./",     exec_argv, exec_envp);
     perror(argv[0]);
   }
   // exit(EXIT_FAILURE);
