e5bd9d8be25a2850dab3ef6107df19dd3b63dd35
tracer-nju <tracer@nju.edu.cn>
2023-09-25 16:57:18 +0800

    Linux LAPTOP-IRKK0LSS 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:57:18 up 1 day, 13:18,  0 users,  load average: 0.23, 0.19, 0.09

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -8,16 +8,16 @@ int main(int argc, char *argv[]) {
   char *exec_envp[] = { "PATH=/bin", NULL, };
   // execve("strace",          exec_argv, exec_envp);
   // execve("/usr/bin/strace", exec_argv, exec_envp);
+  int fd = open("./sperf_tmp.output", O_CREAT|O_WRONLY|O_TRUNC,S_IRWXU);  
   int p = fork();
   if(p < 0){
-
+    perror("create child process error!\n");
   }else if(p > 0){
     wait(NULL);
   }else{
     close(STDOUT_FILENO);
-    open("./sperf_tmp.output", O_CREAT|O_WRONLY|O_TRUNC,S_IRWXU);  
-    // execve("/bin/strace",     exec_argv, exec_envp);
-    // printf("Hello, I am son\n");
+    execve("/bin/strace",     exec_argv, exec_envp);
+    printf("Hello, I am son\n");
   }
   perror(argv[0]);
   exit(EXIT_FAILURE);
