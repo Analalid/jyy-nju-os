cc23f7427d498aca6ec5f438e3cc6fc30a3df4d0
tracer-nju <tracer@nju.edu.cn>
2023-09-25 19:43:43 +0800

    Linux LAPTOP-IRKK0LSS 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:43:43 up 1 day, 16:04,  0 users,  load average: 0.09, 0.07, 0.05

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -9,6 +9,8 @@ int main(int argc, char *argv[]) {
   char *pathVal = getenv("PATH");
   char *path;
   path = (char*)malloc(strlen("PATH=") + strlen(pathVal) + 1);
+    close(2);
+    open("./sperf_tmp.output", O_CREAT|O_WRONLY|O_TRUNC,S_IRWXU);
   strcpy(path, "PATH=");
   strcat(path, pathVal);
   char *exec_envp[] = { path, NULL, };
@@ -23,11 +25,8 @@ int main(int argc, char *argv[]) {
     perror("create child process error!\n");
   }else if(p > 0){
     wait(NULL);
-    
   }else{
     // close(STDOUT_FILENO);
-    close(2);
-    open("./sperf_tmp.output", O_CREAT|O_WRONLY|O_TRUNC,S_IRWXU);
     execve("/bin/strace",     exec_argv, exec_envp);
     perror(argv[0]);
   }
