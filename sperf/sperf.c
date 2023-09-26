d261b2cb3547ddc145e04c06b1f70e2dccdaa7d2
tracer-nju <tracer@nju.edu.cn>
2023-09-26 13:56:37 +0800

    Linux LAPTOP-IRKK0LSS 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:56:37 up 2 days,  3:50,  0 users,  load average: 0.12, 0.07, 0.01

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -86,7 +86,7 @@ int main(int argc, char *argv[]) {
   close(2);
   // open("./sperf_tmp.output", O_CREAT|O_WRONLY|O_TRUNC,S_IRWXU);
   setbuf(stdout, NULL);
-  int fd = open("sperf_tmp.txt", O_CREAT|O_RDWR|O_TRUNC,S_IRWXU);  
+  int fd = open("sperf_tmp.output", O_CREAT|O_RDWR|O_TRUNC,S_IRWXU);  
   
   if(fd < 0) perror("open file faild!\n");
   int p = fork();
