f68313f2720c28e87dbd9a2764f3f9a6803ed99c
tracer-nju <tracer@nju.edu.cn>
2023-09-26 19:40:40 +0800

    Linux LAPTOP-IRKK0LSS 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:40:40 up 2 days,  8:28,  0 users,  load average: 0.07, 0.16, 0.16

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -109,7 +109,7 @@ void putMapByString(char* substring){
     // int hash = hashFunction(syscallName);
     double v = get_HashMap(map, syscallName);
     put_HashMap(map, syscallName, t + v);
-    printf("====%s   %lf\n ",syscallName, t);
+    printf("====%s   %lf\n ",syscallName, v);
     totalTimeCost += t;
 }
 void readTmpOutFile(int fd){
