e8b3625d8b31bd4449957319b14427be4a15b7a6
tracer-nju <tracer@nju.edu.cn>
2023-09-26 16:50:18 +0800

    Linux LAPTOP-IRKK0LSS 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:50:18 up 2 days,  5:38,  0 users,  load average: 0.08, 0.04, 0.04

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -8,7 +8,7 @@
 #include <stdint.h>
 
  #define ARRAY_SIZE(arr) (sizeof((arr)) / sizeof((arr)[0]))
-static const char *const  re_key = ".*\(";
+static const char *const  re_key = ".*";
 static const char *const re_value = "<.*>";
 void insertString(char *str){
   char* key;
