69af3f554caab9651466438571542c4aca038637
Analalid <2315932049@qq.com>
2023-09-26 19:59:48 +0800

    correct all message from strace and build HashMap

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -126,12 +126,7 @@ void readTmpOutFile(int fd){
           index = 0;  // 重置索引
       } else {
           line[index] = ch;
-          if(1){
-          int t = 3&&6&&9;
-          printf("%d\n", t);
-          }
-  // printfMap();
-          // printf("%d\n", 3&6&9);
+          printfMap();
           index++;
       }
   }
