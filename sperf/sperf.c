f78759a78e8ddc0961efa87d5b4bd3fe28585a4b
tracer-nju <tracer@nju.edu.cn>
2023-09-27 21:23:08 +0800

    Linux LAPTOP-IRKK0LSS 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:23:08 up 3 days,  2:19,  0 users,  load average: 0.00, 0.18, 0.29

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -174,7 +174,7 @@ void printfMap(){
     int x_1 = 0, y_1 = 0, x_2 = SYSCALL_INFO_WINDOW_HEIGHT, y_2 = SYSCALL_INFO_WINDOW_WIDTH; 
     for(int i = idx - 1; i >= 0; --i){
       // printf("%d\n ================== \n", i);
-      double percent = dataArr[i]->value / totalTimeCost;
+      double percent = dataArr[i]->value / totalTimeCost + 0.5;
       int endX = -1;
       int endY = -1;
       if(i & 1){
