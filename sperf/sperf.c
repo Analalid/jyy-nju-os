8aa9d9726d378a885e02d6c574825e98bda9a99b
tracer-nju <tracer@nju.edu.cn>
2023-09-27 21:57:49 +0800

    Linux LAPTOP-IRKK0LSS 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:57:49 up 3 days,  2:54,  0 users,  load average: 0.58, 0.52, 0.42

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -191,7 +191,7 @@ void printfMap(){
       // drawBlock(dataArr[i]->key, 100, idx - 1 - i, x_1, y_1, x_2, y_2);
       // if(i != idx - 1){
 
-      if(i == 0 && i == idx - 1 - SYSCALL_INFO_MAX){
+      if(i == 0 && i == idx - 1 - SYSCALL_INFO_MAX - 2){
         // while(1) printf("a\n");
         drawBlock("else", 100, 4, x_1, y_1, x_2, y_2);
       }else drawBlock(dataArr[i]->key, percent * 100, idx - 1 - i, x_1, y_1, endX, endY);
