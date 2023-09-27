1414d5c0b766928e8ec3fd8ff5c95b89be704d99
tracer-nju <tracer@nju.edu.cn>
2023-09-27 18:40:02 +0800

    Linux LAPTOP-IRKK0LSS 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:40:02 up 2 days, 23:36,  0 users,  load average: 0.79, 0.22, 0.07

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -136,7 +136,7 @@ int compareHashEntry(const void* a, const void* b) {
     return entry1->value - entry2->value > 0 ? 1 : -1;
 }
 void drawBlock(char* key, double percent, int idx, int left_top_row, int left_top_col, int right_end_row, int right_end_col){
-  printf("\033[45;m");
+  printf("\033[45;33m");
   for(int i = left_top_row; i < right_end_row; ++i){
     for(int j = left_top_col; j < right_end_col; ++j){ syscall_info_show(idx, " "); printf(" ");}
     syscall_info_show_move_down(1);
