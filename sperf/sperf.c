cd03893018120e67ba4a25b3ccf5e5cba353d79f
tracer-nju <tracer@nju.edu.cn>
2023-09-27 22:02:24 +0800

    Linux LAPTOP-IRKK0LSS 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:02:24 up 3 days,  2:58,  0 users,  load average: 0.28, 0.33, 0.36

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -154,7 +154,8 @@ void drawBlock(char* key, double percent, int idx, int left_top_row, int left_to
       if(i == left_top_row && strlen(result) > right_end_col - left_top_col){
         syscall_info_show_move_left(strlen(result) - (right_end_col - left_top_col));
         // break;
-      }else syscall_info_show_move_left(right_end_col - left_top_col);
+      }
+      syscall_info_show_move_left(right_end_col - left_top_col);
     }
     free(result);
 }
