5963f7abf4f0e8c57413ecf4779c2e15fd738278
tracer-nju <tracer@nju.edu.cn>
2023-09-27 21:03:55 +0800

    Linux LAPTOP-IRKK0LSS 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:03:55 up 3 days,  2:00,  0 users,  load average: 0.33, 0.36, 0.27

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -198,7 +198,7 @@ void printfMap(){
       syscall_info_show_position_init();
       syscall_info_show_move_right(y_1);
       syscall_info_show_move_down(x_1);
-      drawBlock(dataArr[i]->key, percent * 100, idx - 1 - i, endX,endY, x_2, y_2);
+      drawBlock(dataArr[i]->key, percent * 100, idx - 1 - i + 1 , endX,endY, x_2, y_2);
       // printf("d\n");
       break;
       // if(i == idx - 2) break;
