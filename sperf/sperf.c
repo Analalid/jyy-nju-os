623c3074a479a8953b5fdbdc82f7f61f04618d0f
tracer-nju <tracer@nju.edu.cn>
2023-09-27 19:46:36 +0800

    Linux LAPTOP-IRKK0LSS 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:46:36 up 3 days, 43 min,  0 users,  load average: 0.15, 0.13, 0.09

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -150,6 +150,7 @@ void drawBlock(char* key, double percent, int idx, int left_top_row, int left_to
 
     // 释放动态分配的内存
     syscall_info_show(idx, result);
+    syscall_info_show_move_left(strlen(result));
     for(int i = left_top_row; i < right_end_row; ++i){
       for(int j = left_top_col; j < right_end_col; ++j){
         syscall_info_show(idx, " ");
