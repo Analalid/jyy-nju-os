32e150e87bd1527ac7ad50d4feb4d0d9411e1fa5
tracer-nju <tracer@nju.edu.cn>
2023-09-27 20:51:28 +0800

    Linux LAPTOP-IRKK0LSS 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:51:28 up 3 days,  1:47,  0 users,  load average: 0.18, 0.13, 0.11

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -195,8 +195,8 @@ void printfMap(){
       syscall_info_show_position_init();
       syscall_info_show_move_right(y_1);
       syscall_info_show_move_down(x_1);
-      // break;
-      if(i == idx - 2) break;
+      break;
+      // if(i == idx - 2) break;
     }
     // printf("\033[0m");  // 重置文本格式
 };
