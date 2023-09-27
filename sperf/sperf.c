19f20020b2e6d862cd5add85b89ecb6a0622a499
tracer-nju <tracer@nju.edu.cn>
2023-09-27 13:44:22 +0800

    Linux LAPTOP-IRKK0LSS 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:44:22 up 2 days, 18:40,  0 users,  load average: 0.03, 0.15, 0.13

--- sperf/sperf.c
+++ sperf/sperf.c
@@ -163,7 +163,7 @@ void printfMap(){
     fflush(stdout);
     // int x = ;
     for(int i = idx - 1; i >= 0 && i > idx - 1 - SYSCALL_INFO_MAX; --i){
-      drawBlock(dataArr[i]->key, dataArr[i]->value / totalTimeCost, i, 0, 0, 100, 100);
+      drawBlock(dataArr[i]->key, dataArr[i]->value / totalTimeCost, i -idx + 1, 0, 0, 100, 100);
       break;
       // printf("key: %s   value: %lf\n", dataArr[i]->key, dataArr[i]->value);
     }
